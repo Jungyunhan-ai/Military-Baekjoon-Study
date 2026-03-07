264ced401d4378bc5fd728432f1d88547800b44d
dbsgks1023 <yoonalex05@naver.com>
2026-03-07 22:53:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8292 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+check = [0] * 10001
+
+for _ in range(n):
+    num = int(input())
+    check[num] += 1 
+
+for i in range(10001):
+    if check[i] != 0:
+        for _ in range(check[i]):
+            print(i)
+    
